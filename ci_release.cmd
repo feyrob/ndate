7e03f01660540378e4a6e94adb8cdf531cdcf71c
feyrob <feyrob@gmail.com>
2021-07-11 08:08:18 +0100

    test

--- ci_release.cmd
+++ ci_release.cmd
@@ -3,7 +3,7 @@
 @echo. 
 @echo. 
 type app_version.h
-echo "int version = %GITHUB_RUN_NUMBER%;" > app_version.h
+echo int version = %GITHUB_RUN_NUMBER%; > app_version.h
 type app_version.h
 @call %~dp0.\build_exe.cmd
 
