8be9f7a4a729da87770af0acecf98e0c27cb5463
feyrob <feyrob@gmail.com>
2021-07-11 08:03:58 +0100

    test

--- ci_release.cmd
+++ ci_release.cmd
@@ -2,7 +2,7 @@
 @echo. 
 @echo. 
 @echo. 
-echo "int version = %GITHUB_RUN_NUMBER%;" > version.h
+echo "int version = %GITHUB_RUN_NUMBER%;" > app_version.h
 @call %~dp0.\build_exe.cmd
 
 %~dp0.\build_out\%app_name%.exe now utc compat
